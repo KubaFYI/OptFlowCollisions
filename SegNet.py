856e1b600a0b231ed4fb79044c62b8950769c9a2
Kuba Sanak <tech@kuba.fyi>
2018-07-17 13:11:54 +0100

    Adopted the code to use my data + simplified the construction of encoder-decoder

--- SegNet.py
+++ SegNet.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-from keras.models import Model
-from keras.layers import Input
-from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape, Permute
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.merge import Multiply, Concatenate
-from keras.utils import np_utils
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Permute
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Multiply, Concatenate
+# from tensorflow.keras.utils import np_utils
+import tensorflow.keras.backend as K
+from tensorflow.python import debug as tf_debug
 
 from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D
-from generator import data_gen_small
+from generator import data_gen
 
 import os
 import numpy as np
@@ -16,174 +19,131 @@ import argparse
 import json
 import pandas as pd
 from PIL import Image
+import pickle
+import pdb
 
 
-def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2), output_mode="softmax"):
+def CreateSegNet(input_shape, kernel=3, pool_size=(2, 2), output_mode="softmax"):
     # encoder
-    inputs = Input(shape=input_shape)
+    K.set_image_data_format('channels_last')
 
-    conv_1 = Convolution2D(64, (kernel, kernel), padding="same")(inputs)
-    conv_1 = BatchNormalization()(conv_1)
-    conv_1 = Activation("relu")(conv_1)
-    conv_2 = Convolution2D(64, (kernel, kernel), padding="same")(conv_1)
-    conv_2 = BatchNormalization()(conv_2)
-    conv_2 = Activation("relu")(conv_2)
+    inputs = Input(shape=(input_shape[1], input_shape[0], input_shape[2]))
 
-    pool_1, mask_1 = MaxPoolingWithArgmax2D(pool_size)(conv_2)
+    layers = [(2, 64), (2, 128), (3, 256), (3, 512), (3, 512)]
+    layers = [(1, 64), (1, 64)]
+    end_dec, masks = SegNetEncoderDecoderGenerator(inputs, layers=layers,
+                                kernel=3, pool_size=(2, 2),
+                                shave_off_decoder_end=0)
 
-    conv_3 = Convolution2D(128, (kernel, kernel), padding="same")(pool_1)
-    conv_3 = BatchNormalization()(conv_3)
-    conv_3 = Activation("relu")(conv_3)
-    conv_4 = Convolution2D(128, (kernel, kernel), padding="same")(conv_3)
-    conv_4 = BatchNormalization()(conv_4)
-    conv_4 = Activation("relu")(conv_4)
 
-    pool_2, mask_2 = MaxPoolingWithArgmax2D(pool_size)(conv_4)
+    # conv_24 = Convolution2D(64, (kernel, kernel), padding="same")(end_dec)
+    # conv_24 = BatchNormalization()(conv_24)
+    # conv_24 = Activation("relu")(conv_24)
 
-    conv_5 = Convolution2D(256, (kernel, kernel), padding="same")(pool_2)
-    conv_5 = BatchNormalization()(conv_5)
-    conv_5 = Activation("relu")(conv_5)
-    conv_6 = Convolution2D(256, (kernel, kernel), padding="same")(conv_5)
-    conv_6 = BatchNormalization()(conv_6)
-    conv_6 = Activation("relu")(conv_6)
-    conv_7 = Convolution2D(256, (kernel, kernel), padding="same")(conv_6)
-    conv_7 = BatchNormalization()(conv_7)
-    conv_7 = Activation("relu")(conv_7)
+    # print(masks[0].shape)
+    # unpool_5 = MaxUnpooling2D(pool_size)([conv_24, masks[0]])
 
-    pool_3, mask_3 = MaxPoolingWithArgmax2D(pool_size)(conv_7)
+    # conv_25 = Convolution2D(64, (kernel, kernel), padding="same")(unpool_5)
+    # conv_25 = BatchNormalization()(conv_25)
+    # conv_25 = Activation("relu")(conv_25)
 
-    conv_8 = Convolution2D(512, (kernel, kernel), padding="same")(pool_3)
-    conv_8 = BatchNormalization()(conv_8)
-    conv_8 = Activation("relu")(conv_8)
-    conv_9 = Convolution2D(512, (kernel, kernel), padding="same")(conv_8)
-    conv_9 = BatchNormalization()(conv_9)
-    conv_9 = Activation("relu")(conv_9)
-    conv_10 = Convolution2D(512, (kernel, kernel), padding="same")(conv_9)
-    conv_10 = BatchNormalization()(conv_10)
-    conv_10 = Activation("relu")(conv_10)
-
-    pool_4, mask_4 = MaxPoolingWithArgmax2D(pool_size)(conv_10)
-
-    conv_11 = Convolution2D(512, (kernel, kernel), padding="same")(pool_4)
-    conv_11 = BatchNormalization()(conv_11)
-    conv_11 = Activation("relu")(conv_11)
-    conv_12 = Convolution2D(512, (kernel, kernel), padding="same")(conv_11)
-    conv_12 = BatchNormalization()(conv_12)
-    conv_12 = Activation("relu")(conv_12)
-    conv_13 = Convolution2D(512, (kernel, kernel), padding="same")(conv_12)
-    conv_13 = BatchNormalization()(conv_13)
-    conv_13 = Activation("relu")(conv_13)
-
-    pool_5, mask_5 = MaxPoolingWithArgmax2D(pool_size)(conv_13)
-    print("Build enceder done..")
-
-    # decoder
-
-    unpool_1 = MaxUnpooling2D(pool_size)([pool_5, mask_5])
-
-    conv_14 = Convolution2D(512, (kernel, kernel), padding="same")(unpool_1)
-    conv_14 = BatchNormalization()(conv_14)
-    conv_14 = Activation("relu")(conv_14)
-    conv_15 = Convolution2D(512, (kernel, kernel), padding="same")(conv_14)
-    conv_15 = BatchNormalization()(conv_15)
-    conv_15 = Activation("relu")(conv_15)
-    conv_16 = Convolution2D(512, (kernel, kernel), padding="same")(conv_15)
-    conv_16 = BatchNormalization()(conv_16)
-    conv_16 = Activation("relu")(conv_16)
-
-    unpool_2 = MaxUnpooling2D(pool_size)([conv_16, mask_4])
-
-    conv_17 = Convolution2D(512, (kernel, kernel), padding="same")(unpool_2)
-    conv_17 = BatchNormalization()(conv_17)
-    conv_17 = Activation("relu")(conv_17)
-    conv_18 = Convolution2D(512, (kernel, kernel), padding="same")(conv_17)
-    conv_18 = BatchNormalization()(conv_18)
-    conv_18 = Activation("relu")(conv_18)
-    conv_19 = Convolution2D(256, (kernel, kernel), padding="same")(conv_18)
-    conv_19 = BatchNormalization()(conv_19)
-    conv_19 = Activation("relu")(conv_19)
-
-    unpool_3 = MaxUnpooling2D(pool_size)([conv_19, mask_3])
-
-    conv_20 = Convolution2D(256, (kernel, kernel), padding="same")(unpool_3)
-    conv_20 = BatchNormalization()(conv_20)
-    conv_20 = Activation("relu")(conv_20)
-    conv_21 = Convolution2D(256, (kernel, kernel), padding="same")(conv_20)
-    conv_21 = BatchNormalization()(conv_21)
-    conv_21 = Activation("relu")(conv_21)
-    conv_22 = Convolution2D(128, (kernel, kernel), padding="same")(conv_21)
-    conv_22 = BatchNormalization()(conv_22)
-    conv_22 = Activation("relu")(conv_22)
-
-    unpool_4 = MaxUnpooling2D(pool_size)([conv_22, mask_2])
-
-    conv_23 = Convolution2D(128, (kernel, kernel), padding="same")(unpool_4)
-    conv_23 = BatchNormalization()(conv_23)
-    conv_23 = Activation("relu")(conv_23)
-    conv_24 = Convolution2D(64, (kernel, kernel), padding="same")(conv_23)
-    conv_24 = BatchNormalization()(conv_24)
-    conv_24 = Activation("relu")(conv_24)
-
-    unpool_5 = MaxUnpooling2D(pool_size)([conv_24, mask_1])
-
-    conv_25 = Convolution2D(64, (kernel, kernel), padding="same")(unpool_5)
-    conv_25 = BatchNormalization()(conv_25)
-    conv_25 = Activation("relu")(conv_25)
-
-    conv_26 = Convolution2D(n_labels, (1, 1), padding="valid")(conv_25)
-    conv_26 = BatchNormalization()(conv_26)
-    conv_26 = Reshape((input_shape[0] * input_shape[1], n_labels), input_shape=(input_shape[0], input_shape[1], n_labels))(conv_26)
+    # conv_26 = Convolution2D(1, (1, 1), padding="valid")(end_dec)
+    # conv_26 = BatchNormalization()(conv_26)
+    # conv_26 = Reshape((input_shape[0] * input_shape[1], 1), input_shape=(input_shape[0], input_shape[1], 1))(conv_26)
 
     outputs = Activation(output_mode)(conv_26)
-    print("Build decoder done..")
 
-    segnet = Model(inputs=inputs, outputs=outputs, name="SegNet")
+    segnet = Model(inputs=inputs, outputs=outputs, name="OFNet")
 
     return segnet
 
+def SegNetEncoderDecoderGenerator(inputs, layers, kernel=3, pool_size=(2, 2), shave_off_decoder_end=0):
+    '''
+    Creates an convolutional symmetric encoder-decoder architecture similar to
+    that of a SegNet. The exact structure is defined by the 'layers' argument
+    which should be a list of tuples, each tuple representing the number and
+    width of convolutional layers at each stage of encoder/decoder.
+    '''
+
+    masks = []
+
+    io = inputs
+    total_layers = 0
+
+    # encoder
+    for group in layers:
+        layers_no = group[0]
+        width = group[1]
+        for i in range(layers_no):
+            io = Convolution2D(width, (kernel, kernel), padding="same")(io)
+            io = BatchNormalization()(io)
+            io = Activation("relu")(io)
+            total_layers += 1
+
+        io, mask = MaxPoolingWithArgmax2D(pool_size)(io)
+        masks.append(mask)
+
+    print("Build enceder done..")
+
+    # decoder
+    if shave_off_decoder_end > 0:
+        total_layers -= shave_off_decoder_end
+
+    # pdb.set_trace()
+    for idx, group in enumerate(reversed(layers)):
+        layers_no = group[0]
+        width = group[1]
+
+        io = MaxUnpooling2D(pool_size)([io, masks[-1-idx]])
+
+        for i in range(layers_no):
+            io = Convolution2D(width, (kernel, kernel), padding="same")(io)
+            io = BatchNormalization()(io)
+            io = Activation("relu")(io)
+            total_layers -= 1
+            if total_layers <= 0:
+                return io, masks[:-1-idx]
+
 
 def main(args):
-    # set the necessary list
-    train_list = pd.read_csv(args.train_list,header=None)
-    val_list = pd.read_csv(args.val_list,header=None)
+    train_gen = data_gen(args.data_dir, args.batch_size,
+                         timestamp_range=(0.1, 0.8), range_in_fractions=True,
+                         img_resolution=args.input_shape)
+    val_gen = data_gen(args.data_dir, args.batch_size,
+                         timestamp_range=(0.8, 0.9), range_in_fractions=True,
+                         img_resolution=args.input_shape)
 
-    # set the necessary directories
-    trainimg_dir = args.trainimg_dir
-    trainmsk_dir = args.trainmsk_dir
-    valimg_dir = args.valimg_dir
-    valmsk_dir = args.valmsk_dir
 
-    train_gen = data_gen_small(trainimg_dir, trainmsk_dir, train_list, args.batch_size, [args.input_shape[0], args.input_shape[1]], args.n_labels)
-    val_gen = data_gen_small(valimg_dir, valmsk_dir, val_list, args.batch_size, [args.input_shape[0], args.input_shape[1]], args.n_labels)
-
-    segnet = CreateSegNet(args.input_shape, args.n_labels, args.kernel, args.pool_size, args.output_mode)
+    segnet = CreateSegNet(args.input_shape, args.kernel, args.pool_size, args.output_mode)
     print(segnet.summary())
 
     segnet.compile(loss=args.loss, optimizer=args.optimizer, metrics=["accuracy"])
-    segnet.fit_generator(train_gen, steps_per_epoch=args.epoch_steps, epochs=args.n_epochs, validation_data=val_gen, validation_steps=args.val_steps)
 
-    segnet.save_weights("../LIP/pretrained/LIP_SegNet"+str(args.n_epochs)+".hdf5")
+    config = tf.ConfigProto(device_count={'GPU': 0})
+    sess = tf.Session(config=config)
+
+    sess = K.get_session()
+    sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+    K.set_session(sess)
+
+    history = segnet.fit_generator(train_gen, steps_per_epoch=args.epoch_steps, epochs=args.n_epochs, validation_data=val_gen, validation_steps=args.val_steps, workers=0)
+
+    pickle.dump(history.history, open(r'history.pickle', 'wb'))
+
+
+    segnet.save_weights("pretrained/LIP_SegNet"+str(args.n_epochs)+".hdf5")
     print("sava weight done..")
 
     json_string = segnet.to_json()
-    open("../LIP/pretrained/LIP_SegNet.json", "w").write(json_string)
+    open("pretrained/LIP_SegNet.json", "w").write(json_string)
 
 
 if __name__ == "__main__":
     # command line argments
+    training_data_loc = '/home/kubafyi/Code/SegNet-Tutorial/CamVid'
     parser = argparse.ArgumentParser(description="SegNet LIP dataset")
-    parser.add_argument("--train_list",
-            help="train list path")
-    parser.add_argument("--trainimg_dir",
-            help="train image dir path")
-    parser.add_argument("--trainmsk_dir",
-            help="train mask dir path")
-    parser.add_argument("--val_list",
-            help="val list path")
-    parser.add_argument("--valimg_dir",
-            help="val image dir path")
-    parser.add_argument("--valmsk_dir",
-            help="val mask dir path")
+    parser.add_argument("--data_dir",
+            default=os.path.join('/mnt', 'data', 'AirSimCollectedData', 'testing'), help="Training / validation / testing data location")
     parser.add_argument("--batch_size",
             default=10,
             type=int,
@@ -205,7 +165,7 @@ if __name__ == "__main__":
             type=int,
             help="Number of label")
     parser.add_argument("--input_shape",
-            default=(256, 256, 3),
+            default=(144, 256, 2),
             help="Input images shape")
     parser.add_argument("--kernel",
             default=3,
@@ -219,7 +179,7 @@ if __name__ == "__main__":
             type=str,
             help="output activation")
     parser.add_argument("--loss",
-            default="categorical_crossentropy",
+            default="mean_squared_error",
             type=str,
             help="loss function")
     parser.add_argument("--optimizer",
--- generator.py
+++ generator.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
 import numpy as np
+import pandas as pd
 import cv2
-from keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
+import csv
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
 from scipy.misc import imresize
-
+import pdb
 
 def catelab(labels, dims, n_labels):
     x = np.zeros([dims[0], dims[1], n_labels])
@@ -13,24 +16,61 @@ def catelab(labels, dims, n_labels):
     x = x.reshape(dims[0] * dims[1], n_labels)
     return x
 
+# def binarylab(mask, n_labels):
+#     result = np.zeros((n_labels, mask.shape[0], mask.shape[1]))
+#     return result
 
-# generator that we will use to read the data from the directory
-def data_gen_small(img_dir, mask_dir, lists, batch_size, dims, n_labels):
-        while True:
-            ix = np.random.choice(np.arange(len(lists)), batch_size)
-            imgs = []
-            labels = []
-            for i in ix:
-                # images
-                original_img = cv2.imread(img_dir + lists.iloc[i, 0]+".jpg")[:, :, ::-1]
-                resized_img = cv2.resize(original_img, dims+[3])
-                array_img = img_to_array(resized_img)/255
-                imgs.append(array_img)
-                # masks
-                original_mask = cv2.imread(mask_dir + lists.iloc[i, 0] + '.png')
-                resized_mask = cv2.resize(original_mask, (dims[0], dims[1]))
-                array_mask = binarylab(resized_mask[:, :, 0], dims, n_labels)
-                labels.append(array_mask)
-            imgs = np.array(imgs)
-            labels = np.array(labels)
-            yield imgs, labels
+
+csv_col_names = ['LV_x', 'LV_y', 'LV_z', 'AV_x', 'AV_y', 'AV_z', 'OQ_w', 'OQ_x', 'OQ_y', 'OQ_z']
+
+def data_gen(data_dir, batch_size,
+             timestamp_range=None, range_in_fractions=False,
+             img_resolution=None):
+    ''' Generator for data batches from AirSim-generated data. '''
+    print('hey')
+    datapoints_csv_name = os.path.join(data_dir, 'airsim_rec.csv')
+    datapoints_all = pd.read_csv(datapoints_csv_name,
+                             header=None,
+                             sep=',',
+                             names=csv_col_names,
+                             index_col=0)
+
+    # Figure out first and last timestep of the range
+    if timestamp_range is None:
+        timestamp_range = (datapoints_all.index[0], datapoints_all.index[-1])
+
+    if range_in_fractions:
+        start_idx = int(np.floor(timestamp_range[0] * datapoints_all.shape[0]))
+        end_idx = int(np.ceil(timestamp_range[0] * datapoints_all.shape[0]))
+        start_timestamp = datapoints_all.index[start_idx]
+        end_timestamp = datapoints_all.index[end_idx]
+    else:
+        start_timestamp = timestamp_range[0]
+        end_timestamp = timestamp_range[1]
+
+    datapoints = datapoints_all.loc[start_timestamp:end_timestamp]
+
+    while True:
+        print('yo')
+        inputs = []
+        labels = []
+        ix = np.random.choice(np.arange(len(datapoints)), batch_size)
+        for i in ix:
+            # Get the optical flow input
+            opt_flow_n = os.path.join(data_dir, 'images',
+                                      'flow_' + str(datapoints.index[i]) + '.npz')
+            opt_flow = np.load(opt_flow_n)['opt_flow']
+
+            # Get the 'collision distance' labels
+            coll_dist_n = os.path.join(data_dir, 'images',
+                                      'misc_' + str(datapoints.index[i]) + '.npz')
+            coll_dist = np.load(coll_dist_n)['arr_0']
+
+            if img_resolution is not None:
+                # print('{} -> {}'.format(opt_flow.shape[:2], img_resolution[:2]))
+                opt_flow = cv2.resize(opt_flow, img_resolution[:2])
+            inputs.append(opt_flow)
+            labels.append(np.expand_dims(coll_dist, axis=0).T)
+        inputs = np.array(inputs)
+        labels = np.array(labels)
+        yield inputs, labels
