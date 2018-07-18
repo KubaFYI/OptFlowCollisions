9a2920e21f695fc7be7852e0c26042208f8c29e6
Kuba Sanak <tech@kuba.fyi>
2018-07-18 13:34:11 +0100

    Fix creation of parametrized encoder-decoder

--- SegNet.py
+++ SegNet.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
+# tf.enable_eager_execution()
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Permute
 from tensorflow.keras.layers import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Multiply, Concatenate
-# from tensorflow.keras.utils import np_utils
+
 import tensorflow.keras.backend as K
 from tensorflow.python import debug as tf_debug
 
-from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D
+from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D, basic_unpool2d
 from generator import data_gen
 
 import os
@@ -23,33 +24,23 @@ import pickle
 import pdb
 
 
-def CreateSegNet(input_shape, kernel=3, pool_size=(2, 2), output_mode="softmax"):
+def CreateSegNet(input_shape, kernel=3, pool_size=(2, 2), output_mode="softmax", inputz=None):
     # encoder
     K.set_image_data_format('channels_last')
 
-    inputs = Input(shape=(input_shape[1], input_shape[0], input_shape[2]))
+    if inputz is None:
+        inputs = Input(shape=(input_shape[1], input_shape[0], input_shape[2]))
+    else:
+        inputs = tf.stack(inputz[0])
 
-    layers = [(2, 64), (2, 128), (3, 256), (3, 512), (3, 512)]
-    layers = [(1, 64), (1, 64)]
-    end_dec, masks = SegNetEncoderDecoderGenerator(inputs, layers=layers,
+    layers = [(2, 64), (2, 128), (3, 256), (3, 512)]
+    layers = [(2, 64), (2, 128)]
+    enc_dec, masks = SegNetEncoderDecoderGenerator(inputs, layers=layers,
                                 kernel=3, pool_size=(2, 2),
-                                shave_off_decoder_end=0)
+                                shave_off_decoder_end=1)
 
-
-    # conv_24 = Convolution2D(64, (kernel, kernel), padding="same")(end_dec)
-    # conv_24 = BatchNormalization()(conv_24)
-    # conv_24 = Activation("relu")(conv_24)
-
-    # print(masks[0].shape)
-    # unpool_5 = MaxUnpooling2D(pool_size)([conv_24, masks[0]])
-
-    # conv_25 = Convolution2D(64, (kernel, kernel), padding="same")(unpool_5)
-    # conv_25 = BatchNormalization()(conv_25)
-    # conv_25 = Activation("relu")(conv_25)
-
-    # conv_26 = Convolution2D(1, (1, 1), padding="valid")(end_dec)
-    # conv_26 = BatchNormalization()(conv_26)
-    # conv_26 = Reshape((input_shape[0] * input_shape[1], 1), input_shape=(input_shape[0], input_shape[1], 1))(conv_26)
+    conv_26 = Convolution2D(1, (1, 1), padding="same")(enc_dec)
+    conv_26 = BatchNormalization()(conv_26)
 
     outputs = Activation(output_mode)(conv_26)
 
@@ -71,7 +62,7 @@ def SegNetEncoderDecoderGenerator(inputs, layers, kernel=3, pool_size=(2, 2), sh
     total_layers = 0
 
     # encoder
-    for group in layers:
+    for idx, group in enumerate(layers):
         layers_no = group[0]
         width = group[1]
         for i in range(layers_no):
@@ -83,13 +74,12 @@ def SegNetEncoderDecoderGenerator(inputs, layers, kernel=3, pool_size=(2, 2), sh
         io, mask = MaxPoolingWithArgmax2D(pool_size)(io)
         masks.append(mask)
 
-    print("Build enceder done..")
+    print("Building enceder done..")
 
     # decoder
     if shave_off_decoder_end > 0:
         total_layers -= shave_off_decoder_end
 
-    # pdb.set_trace()
     for idx, group in enumerate(reversed(layers)):
         layers_no = group[0]
         width = group[1]
@@ -97,6 +87,13 @@ def SegNetEncoderDecoderGenerator(inputs, layers, kernel=3, pool_size=(2, 2), sh
         io = MaxUnpooling2D(pool_size)([io, masks[-1-idx]])
 
         for i in range(layers_no):
+            # if this is the last convolution in a series, followed up by
+            # unpooling and then a convolution group with a different width, it
+            # has to reduce width of the tensor now
+            if i == layers_no-1 and idx != len(layers)-1:
+                # last layer before UnPooling
+                width = layers[-1-idx-1][1]
+
             io = Convolution2D(width, (kernel, kernel), padding="same")(io)
             io = BatchNormalization()(io)
             io = Activation("relu")(io)
@@ -104,6 +101,7 @@ def SegNetEncoderDecoderGenerator(inputs, layers, kernel=3, pool_size=(2, 2), sh
             if total_layers <= 0:
                 return io, masks[:-1-idx]
 
+    print("Building decoder done..")
 
 def main(args):
     train_gen = data_gen(args.data_dir, args.batch_size,
@@ -119,13 +117,16 @@ def main(args):
 
     segnet.compile(loss=args.loss, optimizer=args.optimizer, metrics=["accuracy"])
 
-    config = tf.ConfigProto(device_count={'GPU': 0})
-    sess = tf.Session(config=config)
+    # Use below to run CPU-only session
+    # config = tf.ConfigProto(device_count={'GPU': 0})
+    # sess = tf.Session(config=config)
 
-    sess = K.get_session()
-    sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-    K.set_session(sess)
+    # Use below to run with the tensorflow debugger
+    # sess = K.get_session()
+    # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+    # K.set_session(sess)
 
+    print("About to train")
     history = segnet.fit_generator(train_gen, steps_per_epoch=args.epoch_steps, epochs=args.n_epochs, validation_data=val_gen, validation_steps=args.val_steps, workers=0)
 
     pickle.dump(history.history, open(r'history.pickle', 'wb'))
