3d940153c8b723eb85eebe7710e60f8d4100c1d2
Kuba Sanak <tech@kuba.fyi>
2018-07-19 12:50:17 +0100

    Misc changes to net creation now enabling proper training

--- SegNet.py
+++ SegNet.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
-# tf.enable_eager_execution()
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, model_from_json
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Permute
 from tensorflow.keras.layers import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
@@ -11,7 +10,7 @@ from tensorflow.keras.layers import Multiply, Concatenate
 import tensorflow.keras.backend as K
 from tensorflow.python import debug as tf_debug
 
-from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D, basic_unpool2d
+from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D
 from generator import data_gen
 
 import os
@@ -24,7 +23,7 @@ import pickle
 import pdb
 
 
-def CreateSegNet(input_shape, kernel=3, pool_size=(2, 2), output_mode="softmax", inputz=None):
+def CreateSegNet(input_shape, kernel=3, pool_size=(2, 2), output_mode="sigmoid", inputz=None):
     # encoder
     K.set_image_data_format('channels_last')
 
@@ -34,15 +33,15 @@ def CreateSegNet(input_shape, kernel=3, pool_size=(2, 2), output_mode="softmax",
         inputs = tf.stack(inputz[0])
 
     layers = [(2, 64), (2, 128), (3, 256), (3, 512)]
-    layers = [(2, 64), (2, 128)]
     enc_dec, masks = SegNetEncoderDecoderGenerator(inputs, layers=layers,
                                 kernel=3, pool_size=(2, 2),
                                 shave_off_decoder_end=1)
 
     conv_26 = Convolution2D(1, (1, 1), padding="same")(enc_dec)
-    conv_26 = BatchNormalization()(conv_26)
+    outputs = BatchNormalization()(conv_26)
 
-    outputs = Activation(output_mode)(conv_26)
+    # For some reason the final activation sets all tensor elements to 1.0 -- why??
+    # outputs = Activation(output_mode)(outputs)
 
     segnet = Model(inputs=inputs, outputs=outputs, name="OFNet")
 
@@ -105,17 +104,28 @@ def SegNetEncoderDecoderGenerator(inputs, layers, kernel=3, pool_size=(2, 2), sh
 
 def main(args):
     train_gen = data_gen(args.data_dir, args.batch_size,
-                         timestamp_range=(0.1, 0.8), range_in_fractions=True,
+                         timestamp_range=(0.0, 0.8), range_in_fractions=True,
                          img_resolution=args.input_shape)
     val_gen = data_gen(args.data_dir, args.batch_size,
-                         timestamp_range=(0.8, 0.9), range_in_fractions=True,
+                         timestamp_range=(0.8, 1.0), range_in_fractions=True,
                          img_resolution=args.input_shape)
 
-
-    segnet = CreateSegNet(args.input_shape, args.kernel, args.pool_size, args.output_mode)
+    if args.continue_training:
+        # Load model and weights
+        print("Loading Data From saved weights")
+        model_dir = os.path.join('pretrained')
+        with open(os.path.join(model_dir, 'LIP_SegNet.json'), 'r') as model_json:
+            custom_layers = {'MaxPoolingWithArgmax2D': MaxPoolingWithArgmax2D,
+                             'MaxUnpooling2D': MaxUnpooling2D}
+            segnet = model_from_json(model_json.read(), custom_layers)
+            segnet.load_weights(os.path.join(model_dir, 'LIP_SegNet10.hdf5'))
+    else:
+        #  Create fresh model
+        segnet = CreateSegNet(args.input_shape, args.kernel, args.pool_size, args.output_mode)
+    
     print(segnet.summary())
 
-    segnet.compile(loss=args.loss, optimizer=args.optimizer, metrics=["accuracy"])
+    segnet.compile(loss=args.loss, optimizer=args.optimizer, metrics=["mean_squared_error"])
 
     # Use below to run CPU-only session
     # config = tf.ConfigProto(device_count={'GPU': 0})
@@ -127,7 +137,7 @@ def main(args):
     # K.set_session(sess)
 
     print("About to train")
-    history = segnet.fit_generator(train_gen, steps_per_epoch=args.epoch_steps, epochs=args.n_epochs, validation_data=val_gen, validation_steps=args.val_steps, workers=0)
+    history = segnet.fit_generator(train_gen, steps_per_epoch=args.epoch_steps, epochs=args.n_epochs, validation_data=val_gen, validation_steps=args.val_steps)
 
     pickle.dump(history.history, open(r'history.pickle', 'wb'))
 
@@ -184,9 +194,12 @@ if __name__ == "__main__":
             type=str,
             help="loss function")
     parser.add_argument("--optimizer",
-            default="adadelta",
+            default="Adam",
             type=str,
-            help="oprimizer")
+            help="optimizer")
+    parser.add_argument("--continue_training",
+            action="store_true",
+            help="Load previously done weights?")
     args = parser.parse_args()
 
     main(args)
