Fixed crossentropy loss calculation and added pixelwise weighting

SegNet.py
@@ -27,7 +27,7 @@ from tensorflow.python import debug as tf_debug
 from Mylayers import MaxPoolingWithArgmax2D
 from Mylayers import MaxUnpooling2D
 from Mylayers import CombineMotionWithImg
-from generator import data_gen, default_bins
+from generator import data_gen, default_bins, calc_class_weights
 
 from datetime import datetime
 import argparse
@@ -99,9 +99,10 @@ def CreateSegNet(input_shapes, kernel=3, pool_size=(2, 2), output_mode="softmax"
                                 shave_off_decoder_end=1)
 
     conv_26 = Convolution2D(nlabels, (1, 1), padding="same")(enc_dec)
-    outputs = BatchNormalization()(conv_26)
+    conv_26 = BatchNormalization()(conv_26)
 
     # For some reason the final activation sets all tensor elements to 1.0 -- why??
+    outputs = Reshape((input_shapes[0][0] * input_shapes[0][1], nlabels), input_shape=(input_shapes[0][0], input_shapes[0][1], nlabels))(conv_26)
     outputs = Activation(output_mode)(outputs)
 
     segnet = Model(inputs=[input_optical_flow, input_motion], outputs=outputs, name="OFNet")
@@ -165,6 +166,35 @@ def SegNetEncoderDecoderGenerator(inputs, layers, pool_size=(2, 2), shave_off_de
 
     print("Building decoder done..")
 
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+
+    (adapted from gist to work with multiclass-per-sample https://gist.github.com/wassname/ce364fddfc8a025bfab4348cf5de852d)
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+    
+    weights = K.variable(weights)
+        
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+    
+    return loss
+
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
@@ -188,14 +218,12 @@ def main(args):
                          scrambled_range=(0.0, 0.9, seed), range_in_fractions=True,
                          img_resolution=args.input_shape, min_max_value=0.1,
                          include_motion_data=True,
-                         bins=bins,
-                         force_metadata_refresh=args.force_metadata_refresh)
+                         bins=bins)
     val_gen = data_gen(args.data_dir, args.batch_size,
                          scrambled_range=(0.9, 1.0, seed), range_in_fractions=True,
                          img_resolution=args.input_shape, min_max_value=0.1,
                          include_motion_data=True,
-                         bins=bins, 
-                         force_metadata_refresh=args.force_metadata_refresh)
+                         bins=bins)
     motion_data_size = (1, 1, 3)
 
     checkpoint = None
@@ -254,12 +282,20 @@ def main(args):
     print("About to train")
     checkpoint_cb = ModelCheckpoint(auto_checkpoint_name, monitor='val_loss', verbose=1, save_best_only=False, mode='min')
     tensorboard_cb = TensorBoard(log_dir='/media/disk1/kuba/tensorboard', histogram_freq=0, write_graph=True, write_images=True)
+
+    class_weights = calc_class_weights(args.data_dir,
+                                       force_metadata_refresh=args.force_metadata_refresh,
+                                       bins=bins)
+    print('Using class weights {}'.format(class_weights))
+    loss_fnctn = weighted_categorical_crossentropy(class_weights)
+
     history = segnet_to_use.fit_generator(train_gen,
                                    steps_per_epoch=args.epoch_steps,
                                    epochs=args.n_epochs,
                                    validation_data=val_gen,
                                    validation_steps=args.val_steps,
-                                   callbacks=[checkpoint_cb, tensorboard_cb])
+                                   callbacks=[checkpoint_cb])
+                                   # callbacks=[checkpoint_cb, tensorboard_cb])
 
     pickle.dump(history.history, open(r'history.pickle', 'wb'))
 

generator.py
@@ -51,13 +51,10 @@ def o_f_compensate_for_rotation(opt_flow, rot, focal=cam_f, xy_scaling=xy_scale_
     xs = np.tile(xs, (current_size[0], 1))
     xs *= xy_scaling
 
-    # import pdb; pdb.set_trace()
-
     # In AirSim Coordinates rot[0] is rotation around the central axis (roll)
     # rot[1] is pitch and rot[2] is negative yaw
 
     # First channel is the horizontal motion component
-    # print('{}\t{}\t{}'.format(rot[1], rot[2], rot[0]))
     opt_flow_corrected[..., 0] -= ys * xs * -rot[1]
     opt_flow_corrected[..., 0] -= -(focal * focal + xs * xs) * -rot[2]
     opt_flow_corrected[..., 0] -= ys * focal * rot[0]
@@ -94,7 +91,9 @@ def world2camera_coords(vect, cam_orient):
         ret
     return ret
 
-def load_optical_flow_metadata(data_dir, datapoints, force_metadata_refresh=False):
+def load_optical_flow_metadata(data_dir, datapoints, force_metadata_refresh=False, bins=None):
+    if bins is None:
+        bins = default_bins
     metadata_file = os.path.join(data_dir, 'optical_flow_metadata.pickle')
     if os.path.isfile(metadata_file) and not force_metadata_refresh:
         f = open(metadata_file, 'rb')
@@ -108,6 +107,7 @@ def load_optical_flow_metadata(data_dir, datapoints, force_metadata_refresh=Fals
         metadata['cam_lin_vel'] = []
         metadata['cam_ang_vel'] = []
         metadata['timestamp_idx'] = {}
+        metadata['class_count'] = []
         vel = []
         orient = []
         idx = 0
@@ -122,6 +122,16 @@ def load_optical_flow_metadata(data_dir, datapoints, force_metadata_refresh=Fals
             orient.append(Quaternion(datapoints.loc[timestamp][['OQ_w', 'OQ_x', 'OQ_y', 'OQ_z']]))
             metadata['cam_ang_vel'].append(datapoints.loc[timestamp][['AV_x', 'AV_y', 'AV_z']])
             metadata['timestamp_idx'][timestamp] = idx
+
+            class_count = []
+            for idx, a_bin in enumerate(bins):
+                if idx != len(bins)-1:
+                    upper = bins[idx+1]
+                else:
+                    upper = 1.
+                class_count.append(np.sum(np.logical_and(coll_dist > a_bin, coll_dist < upper)))
+            metadata['class_count'].append(class_count)
+
             idx += 1
 
         # Calculate the velocity w.r.t. camera
@@ -130,10 +140,26 @@ def load_optical_flow_metadata(data_dir, datapoints, force_metadata_refresh=Fals
         metadata['sums'] = np.array(metadata['sums'])
         metadata['max_vals'] = np.array(metadata['max_vals'])
         metadata['cam_ang_vel'] = np.array(metadata['cam_ang_vel'])
+        metadata['class_count'] = np.array(metadata['class_count'])
 
         f = open(metadata_file, 'wb')
         pickle.dump(metadata, f, protocol=pickle.HIGHEST_PROTOCOL)
     return metadata
+
+def calc_class_weights(data_dir, force_metadata_refresh=False, bins=None):
+    datapoints_csv_name = os.path.join(data_dir, 'airsim_rec.csv')
+    datapoints_all = pd.read_csv(datapoints_csv_name,
+                             header=None,
+                             sep=',',
+                             names=csv_col_names,
+                             index_col=0)
+    metadata = load_optical_flow_metadata(data_dir, datapoints_all,
+                                              force_metadata_refresh=force_metadata_refresh,
+                                              bins=bins)
+
+    class_totals = np.sum(metadata['class_count'], axis=0)
+    weights = np.sum(class_totals) / (class_totals.shape[0] * class_totals)
+    return weights
 
 def data_gen(data_dir, batch_size, bins=None,
              scrambled_range=None,
@@ -154,7 +180,9 @@ def data_gen(data_dir, batch_size, bins=None,
                              names=csv_col_names,
                              index_col=0)
     if min_sum_percentile or min_max_value or include_motion_data:
-        metadata = load_optical_flow_metadata(data_dir, datapoints_all, force_metadata_refresh)
+        metadata = load_optical_flow_metadata(data_dir, datapoints_all,
+                                              force_metadata_refresh=force_metadata_refresh,
+                                              bins=bins)
     # If needed, take only those datapoints where there is most collision
     # probability
     if min_sum_percentile is not None:
@@ -210,6 +238,7 @@ def data_gen(data_dir, batch_size, bins=None,
                                       'misc_' + str(timestamp) + '.npz')
             coll_dist = np.load(coll_dist_n)['arr_0']
             coll_dist *= coll_dist_mul  # not sure why the max value seems to be bounded at 0.5...
+            coll_dist = coll_dist.flatten()
 
             if include_rgb:
                 rgb_n = os.path.join(data_dir, 'images',
@@ -251,13 +280,12 @@ def bin_pixels(data, bins):
     if bins is None:
         return data
     bin_indices = np.digitize(data, bins) - 1
-    result = np.zeros((data.shape[0], data.shape[1], data.shape[2], len(bins)))
+    result = np.zeros((data.shape[0], data.shape[1], len(bins)))
     
     for bin_idx in range(len(bins)):
         one_hot_indices = np.nonzero(bin_indices==bin_idx)
         one_hot_indices = (one_hot_indices[0],
                            one_hot_indices[1],
-                           one_hot_indices[2],
                            bin_idx * np.ones_like(one_hot_indices[0]))
         result[one_hot_indices] = 1
     # print('r{}'.format(result.shape))
@@ -367,7 +395,6 @@ def find_focal_len(data_dir):
     return res
 
 def avg_horizontal_flow(inp, datapoints):
-    # import pdb; pdb.set_trace()
     corr_opt_flow_cumul = 0
     for i in range(len(datapoints)):
         opt_flow_n = os.path.join(data_dir, 'images',

predict.py
@@ -51,7 +51,7 @@ def lout(no):
     print('{}\n{}'.format(res,loaded_model.layers[no]))
 
 
-def one_hot_to_img(img, bins):
+def one_hot_to_img(img, bins, img_shape):
     '''
     Convert a one-hot encoded (or softmaxed probabilities) image of binned
     values into grayscale.
@@ -61,7 +61,16 @@ def one_hot_to_img(img, bins):
     for bin_idx, bin_val in enumerate(bins):
         retimg[np.nonzero(bin_idxs==bin_idx)] = bin_val
     retimg *= 255
-    return retimg
+
+    # Finally, unflatten
+
+    if len(retimg.shape) > 1:
+        unflattented = np.empty((retimg.shape[0], img_shape[0], img_shape[1]), dtype=retimg.dtype)
+        for idx in range(retimg.shape[0]):
+            unflattented[idx, ...] = np.reshape(retimg[idx, ...], img_shape)
+    else:
+        unflattented = np.reshape(retimg, img_shape)
+    return unflattented
 
 
 
@@ -69,9 +78,9 @@ def visualize_prediction(datapoint, display=True):
     inputs, ground_truth, rgbs, timestamps = datapoint
     predictions = loaded_model.predict(inputs)
     inputs = inputs[0]
-
-    ground_truth = one_hot_to_img(ground_truth, bins)
-    predictions = one_hot_to_img(predictions, bins)
+    
+    ground_truth = one_hot_to_img(ground_truth, bins, inputs.shape[-3:-1])
+    predictions = one_hot_to_img(predictions, bins, inputs.shape[-3:-1])
 
     predictions = predictions.astype(np.uint8)
     ground_truth = ground_truth.astype(np.uint8)